18a559791057acc426e212b85762ab12dec0b81d
Jhonathan de Lima <167260048+jhonathancodes@users.noreply.github.com>
2025-08-08 09:26:14 -0300

    Update DarkModeContext.jsx

--- src/contexts/DarkModeContext.jsx
+++ src/contexts/DarkModeContext.jsx
@@ -1,20 +1,19 @@
-// src/contexts/DarkModeContext.jsx
+
 import { createContext, useContext, useState, useEffect } from 'react';
 
 const DarkModeContext = createContext();
 
 export function DarkModeProvider({ children }) {
   const [darkMode, setDarkMode] = useState(() => {
-    // Verifica se há preferência salva no localStorage
+   
     const savedMode = localStorage.getItem('darkMode');
     return savedMode ? JSON.parse(savedMode) : false;
   });
 
   useEffect(() => {
-    // Aplica a classe no body sempre que darkMode mudar
+
     document.body.classList.toggle("dark-mode", darkMode);
     
-    // Salva a preferência no localStorage
     localStorage.setItem('darkMode', JSON.stringify(darkMode));
   }, [darkMode]);
 
@@ -35,4 +34,5 @@ export function useDarkMode() {
     throw new Error('useDarkMode deve ser usado dentro de um DarkModeProvider');
   }
   return context;
+
 }
